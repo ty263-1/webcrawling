eae929226227c85cab67564a5e5bc6b6d71a2b25
ty263-2 <A2s-++ag>
2021-04-07 00:07:05 +0800

    A

--- audiobooks/audiobooks/settings.py
+++ audiobooks/audiobooks/settings.py
@@ -47,13 +47,13 @@ DEFAULT_REQUEST_HEADERS = {
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 #SPIDER_MIDDLEWARES = {
-#    'audiobooks.middlewares.DajiangdaheSpiderMiddleware': 543,
+#    'audiobooks.middlewares.AudiobookSpiderMiddleware': 543,
 #}
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #DOWNLOADER_MIDDLEWARES = {
-#    'audiobooks.middlewares.DajiangdaheDownloaderMiddleware': 543,
+#    'audiobooks.middlewares.AudiobookDownloaderMiddleware': 543,
 #}
 
 # Enable or disable extensions
